822dabba6cc3cb562790321da6f73f0eb44b124a
AlejandroPinto99 <00156618@uca.edu.sv>
2021-12-23 17:55:51 -0600

    Crating cards components

---
+++ components/ExpandingCards/ExpandingCards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpandingCards = () => {
+    return(
+        <div>
+
+        </div>
+    );
+}
+
+export default ExpandingCards;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,15 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div>
-       <p>Im ready to code!</p> 
+    <div> 
+      <div className="text-center">
+          <h1 className="text-5xl mb-4">Interesting example components for websites!</h1>
+          <p>With Pokemon Thematic!! </p>
+      </div>
+      <div className="mt-8 mx-4">
+          <p>Expanding Cards</p>
+      </div>
+     
     </div>
   )
 }
