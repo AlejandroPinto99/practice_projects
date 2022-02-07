9ab06049aa12aa143d9c8323a0e422cfc9ec5831
AlejandroPinto99 <00156618@uca.edu.sv>
2022-02-07 12:24:31 -0600

    Updating progress bar

--- components/ProgressBar/Steps.jsx
+++ components/ProgressBar/Steps.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import StepsStyle from './Steps.module.css'
 
 const Steps = ({ steps }) => {
     const [count, setCount] = useState(1);
@@ -25,28 +26,31 @@ const Steps = ({ steps }) => {
 
     return (
         <div>
-            <div className="relative flex justify-between items-center mt-24 mx-12">
+            <div  className={StepsStyle.bar_container}>
                 {
                     StepsList.length !== 0 && (StepsList.map((item, index) => {
                         return (
                             <div    key={index} 
-                                    className={` rounded-full transition border-4 px-2 z-10 bg-white ${count >= (index + 1 )? 'border-red-600 delay-500 animate-spin animation-delay-500' : 'border-gray-300 opacity-70'}`}
+                                    className={`${StepsStyle.steps}
+                                    ${count >= (index + 1 )? 
+                                        StepsStyle.active_step : 
+                                        StepsStyle.unactive_space}`}
                                     style={{backgroundImage: `url(https://wallpaperaccess.com/full/45634.png)`, 
                                     backgroundSize: 'cover', 
                                     backgroundRepeat: 'no-repeat',
                                     backgroundPositionX: '50%'}} 
                                     >
-                                <p className="invisible">{item}</p>
+                                <p >{item}</p>
                             </div>)
                     }))
                 }
-                <div className="absolute z-0 h-1 bg-gray-300 left-0" style={{width: "100%"}}/>
-                <div className="absolute transition-all duration-700 ease-in-out z-0 h-1 bg-red-600 left-0" style={{width: `${((count - 1)/ (steps - 1))*100}%`}}/>
+                <div className={StepsStyle.empty_line}/>
+                <div className={StepsStyle.filled_line} style={{width: `${((count - 1)/ (steps - 1))*100}%`}}/>
             </div>
-            <div className="flex justify-center mt-8 space-x-4">
-                <button className={`border-2 py-2 px-4 rounded-lg text-white bg-red-600 ${count === 1 ? 'bg-opacity-50 cursor-not-allowed' : 'cursor-pointer hover:bg-red-700'} `}
+            <div className={StepsStyle.btn_container}>
+                <button className={`${StepsStyle.btn_step} ${count === 1 ? StepsStyle.unactive_btn : StepsStyle.active_btn} `}
                         onClick={() => handlePrev()}> Prev </button>
-                <button className={`border-2 py-2 px-4 rounded-lg text-white bg-red-600 ${count === steps ? 'bg-opacity-50 cursor-not-allowed' : 'cursor-pointer hover:bg-red-700'}`}
+                <button className={`${StepsStyle.btn_step} ${count === steps ? StepsStyle.unactive_btn : StepsStyle.active_btn}`}
                         onClick={() => handleNext()}> Next </button>
             </div>
         </div>
