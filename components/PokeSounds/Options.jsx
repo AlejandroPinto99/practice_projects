c53a5715d37cd884d87550a578454effb0234957
AlejandroPinto99 <00156618@uca.edu.sv>
2022-02-18 08:47:07 -0600

    PokeSound

--- components/PokeSounds/Options.jsx
+++ components/PokeSounds/Options.jsx
@@ -1,49 +1,55 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect} from 'react';
+
 import OptionButton from './OptionButton'
 
 //Styles
 import Styles from './PokeSound.module.css'
 
-const Options = ({correct, w1, w2, w3}) => {
+const Options = ({options, correct, points, setPoints, mistakes, setMistakes}) => {
     const [selection, setSelection] = useState();
     const [answer, setAnswer] = useState();
 
     useEffect(() => {
         if(correct)
-            VerifyAnswer();
-    }, [selection])
+            VerifySeleccion();
+    }, [selection]);
 
-    const VerifyAnswer = () => {
-        if(selection === correct.name){
+    useEffect(() => {
+        if(options)
+            setSelection(null)
+    }, [options])
+
+    const VerifySeleccion = () => {
+        if(correct.name === selection){
             setAnswer(true);
-        } else {
+            setPoints(points + 1);
+        } else
+        {
             setAnswer(false);
+            setMistakes(mistakes + 1);
         }
     }
-
+    
     return(
         <div>
             {
-                w1 && (
-                    <div className={Styles.options_container}>
-                    <button className={`${Styles.card_container} ${selection ? Styles.answer_right : ''}`}  
-                    onClick={() => setSelection(correct.name)}>
-                        <OptionButton pokemon={correct} />
-                    </button>
-                    <button className={`${Styles.card_container} ${selection === w1.name ? Styles.answer_wrong: ''}`} 
-                    onClick={() => setSelection(w1.name)}>
-                        <OptionButton pokemon={w1}  />
-                    </button>
-                    <button className={`${Styles.card_container} ${selection === w2.name ? Styles.answer_wrong: ''}`} 
-                    onClick={() => setSelection(w2.name)}>
-                        <OptionButton pokemon={w2}  />
-                    </button>
-                    <button className={`${Styles.card_container} ${selection === w3.name ? Styles.answer_wrong: ''}`} 
-                    onClick={() => setSelection(w3.name)}>
-                        <OptionButton pokemon={w3}  /> 
-                </button>
-            </div>   
+                options && correct && (
+                <div id="options_container" className={Styles.options_container}>
+                    {
+                    options.map((option, index) => {
+                        return(
+                            <button key={index} className={`${Styles.card_container} 
+                            ${selection && correct.name === option.name ? Styles.answer_right : ''}`}
+                                onClick={() => setSelection(option.name)}>
+                                <OptionButton pokemon={option} />
+                            </button>
+                            )
+                        }
+                    )
+                    }
+                </div>
                 )
+            
             }
 
         </div>
--- pages/PokeSounds/index.tsx
+++ pages/PokeSounds/index.tsx
@@ -12,29 +12,39 @@ import PokeStyle from '../../components/PokeSounds/PokeSound.module.css'
 
 const PokeSounds : NextPage = () => {
     const [correct, setCorrect] = useState();
-    const [options, setOptions] = useState([''])
+    const [options, setOptions] = useState([]);
+    const [points, setPoints] = useState(0);
+    const [mistakes, setMistakes] = useState(0);
 
     useEffect(() => {
-        GetPokemonCry();
         GetOptions();
     }, []);
 
+    useEffect(() => {
+        GetPokemonCry();
+    }, [options]);
 
-    async function GetPokemonCry() {
-        const res = await fetch(
-            `https://pokeapi.co/api/v2/pokemon/${Math.round(Math.random()*(151 - 1) + 1)}`
-        );
-            
-        const json = await res.json();   
-        
-        setCorrect(json);
+
+    const GetPokemonCry = () => {
+        setCorrect(options[Math.round(Math.random()*(3 - 0))])
+     
+    }
+
+    const NextRound = () => {
+        GetOptions();
+    }
+
+    const Restart = () => {
+        setPoints(0);
+        setMistakes(0);
+        GetOptions();
     }
 
     async function GetOptions() {
         const json = [];
-        for(let i = 0; i <= 2; i++ ){
+        for(let i = 0; i <= 3; i++ ){
             const res = await fetch(
-                `https://pokeapi.co/api/v2/pokemon/${Math.round(Math.random()*(151 - 1) + 1)}`
+                `https://pokeapi.co/api/v2/pokemon/${Math.round(Math.random()*(151 - 1))}`
             );
             
             json.push(await res.json());
@@ -45,10 +55,31 @@ const PokeSounds : NextPage = () => {
     }
 
     return(
-        <div className={PokeStyle.container}>
-            <SoundButton correct={correct}/>
-            <Options correct={correct} w1={options[0]} w2={options[1]} w3={options[2]} /> 
+        <div >
+            {
+              options  && (
+            <div className={PokeStyle.container}>
+                <SoundButton correct={correct}/>
+                <Options options={options} correct={correct} points={points} mistakes={mistakes} 
+                setMistakes={setMistakes} setPoints={setPoints} />  
+                <div className={PokeStyle.next_container}>
+                    <div>
+                        <button className={PokeStyle.restart_btn} onClick={() => Restart()}>
+                            RESTART
+                        </button>
+                        <button className={PokeStyle.next_btn} onClick={() => NextRound()}>
+                            NEXT
+                        </button>
+                    </div> 
 
+                    <div>
+                        <p> Points: {points}</p>
+                        <p> Mistakes: {mistakes} </p>
+                    </div> 
+                </div>
+            </div>
+              )  
+            }
         </div>
     );
 };
